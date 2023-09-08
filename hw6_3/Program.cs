6889f7f0cc8cdff814488e98abb013bf7672da18
Maria <ec39sp@yandex.ru>
2023-09-09 01:18:48 +0200

    2

---
+++ hw6_3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+*/
+using System.Diagnostics;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите элементы массива, разделенные пробелом:");
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+        double[] array = new double[inputArray.Length];
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (!double.TryParse(inputArray[i].Replace('.', ','), out array[i]))
+            {
+                Console.WriteLine("В массиве должны быть вещественные числа");
+                return;
+            }
+        }
+
+        MaxMinFunc(array);
+    }
+
+    static void MaxMinFunc(double[] array)
+    {
+        double max_i = array[0];
+        double min_i = array[0];
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] > max_i)
+            {
+                max_i = array[i];
+            }
+            else if (array[i] < min_i)
+            {
+                min_i = array[i];
+            }
+        }
+
+        double result = max_i - min_i;
+        Console.WriteLine("Разница между максимальным и минимальным числами: " + result);
+    }
+}
